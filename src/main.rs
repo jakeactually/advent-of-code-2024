d3ec93817251ad5f2dc19f55ae858046dd3abba6
Jeyko Caicedo <jakeactually@outlook.com>
2026-07-09 16:23:19 -0500

    aoc setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
+mod day01;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let day = args[1].as_str();
+    let part = args[2].as_str();
+
+    match (day, part) {
+        ("01", "a") => day01::a::run(),
+        _ => panic!("Unknown day or part"),
+    }
 }
