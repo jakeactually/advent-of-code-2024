fb18019d6c6067f6f4869e59361e7bb5b0b55e3d
Jeyko Caicedo <jakeactually@outlook.com>
2026-07-09 16:35:57 -0500

    change main type to result

--- src/day01/a.rs
+++ src/day01/a.rs
@@ -1,3 +1,13 @@
-pub fn run() -> () {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn run() -> Result<(), String> {
+    let mut file = File::open("src/day01/input.txt").map_err(|e| e.to_string())?;
+
+    let mut s = String::new();
+    file.read_to_string(&mut s).map_err(|e| e.to_string())?;
+
+    println!("{}", s);
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,12 @@ fn main() {
     let day = args[1].as_str();
     let part = args[2].as_str();
 
-    match (day, part) {
+    let res = match (day, part) {
         ("01", "a") => day01::a::run(),
-        _ => panic!("Unknown day or part"),
+        _ => Err("Unknown day or part".to_string()),
+    };
+
+    if let Err(e) = res {
+        println!("{}", e);
     }
 }
