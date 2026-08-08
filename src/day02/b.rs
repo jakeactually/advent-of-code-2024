0c48b7471674fded89b375b14ecdc3b36678c3e2
Jeyko Caicedo <jakeactually@outlook.com>
2026-08-07 21:27:16 -0500

    day 02 b setup

--- src/day02/b.rs
+++ src/day02/b.rs
@@ -6,5 +6,27 @@ pub fn run() -> Result<(), String> {
     let mut text = String::new();
     file.read_to_string(&mut text).map_err(|e| e.to_string())?;
 
+    let mut valid = 0;
+
+    for line in text.lines() {
+        let digits: Vec<i32> = line.split(" ").map(|x| x.parse().unwrap()).collect();
+        let mut diffs = Vec::new();
+
+        for i in 0..digits.len() - 1 {
+            let diff = digits[i] - digits[i + 1];
+            diffs.push(diff);
+        }
+
+        let in_limits = diffs.iter().all(|x| (1..4).contains(&x.abs()));
+        let descending = diffs.iter().all(|x| *x > 0);
+        let ascending = diffs.iter().all(|x| *x < 0);
+
+        if in_limits && (descending || ascending) {
+            valid += 1;
+        }
+    }
+
+    println!("{}", valid);
+
     Ok(())
 }
