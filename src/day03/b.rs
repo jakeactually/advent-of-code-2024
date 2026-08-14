1b8e802ac792f0068d1f8342d730bcb37dfa2c26
Jeyko Caicedo <jakeactually@outlook.com>
2026-08-14 17:18:39 -0500

    day 03 b

--- src/day03/b.rs
+++ src/day03/b.rs
@@ -7,12 +7,22 @@ pub fn run() -> Result<(), String> {
     let mut text = String::new();
     file.read_to_string(&mut text).map_err(|e| e.to_string())?;
 
-    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let re = Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)").unwrap();
     let mut total = 0;
+    let mut enabled = true;
 
-
-    for (_, [a, b]) in re.captures_iter(&text).map(|c| c.extract()) {
-        total += a.parse::<i32>().unwrap() * b.parse::<i32>().unwrap()
+    for captures in re.captures_iter(&text) {
+        if captures.get_match().as_str() == "do()" {
+            enabled = true;
+        } else if captures.get_match().as_str() == "don't()" {
+            enabled = false;
+        } else {
+            let a= captures.get(1).unwrap().as_str();
+            let b= captures.get(2).unwrap().as_str();
+            if enabled {
+                total += a.parse::<i32>().unwrap() * b.parse::<i32>().unwrap();
+            }
+        }
     }
 
     println!("{}", total);
