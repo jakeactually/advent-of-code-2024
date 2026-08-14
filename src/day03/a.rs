ad9e148fa3e92e2914bcf1a6a2abb28a78a65047
Jeyko Caicedo <jakeactually@outlook.com>
2026-08-14 17:00:02 -0500

    day 03 a

--- src/day03/a.rs
+++ src/day03/a.rs
@@ -1,10 +1,21 @@
 use std::fs::File;
 use std::io::prelude::*;
+use regex::Regex;
 
 pub fn run() -> Result<(), String> {
     let mut file = File::open("src/day03/input.txt").map_err(|e| e.to_string())?;
     let mut text = String::new();
     file.read_to_string(&mut text).map_err(|e| e.to_string())?;
+
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let mut total = 0;
+
+
+    for (_, [a, b]) in re.captures_iter(&text).map(|c| c.extract()) {
+        total += a.parse::<i32>().unwrap() * b.parse::<i32>().unwrap()
+    }
+
+    println!("{}", total);
 
     Ok(())
 }
