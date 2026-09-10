96e338d21e098928370ae608d0c47847900d5fb0
Jeyko Caicedo <jakeactually@outlook.com>
2026-09-09 20:05:51 -0500

    day  b setup

--- src/day05/b.rs
+++ src/day05/b.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -6,5 +7,33 @@ pub fn run() -> Result<(), String> {
     let mut text = String::new();
     file.read_to_string(&mut text).map_err(|e| e.to_string())?;
 
+    let (tuples_str, pages_str) = text.split_once("\n\n").unwrap();
+
+    let tuples = tuples_str
+        .lines()
+        .map(|x| x.split_once("|").unwrap())
+        .collect::<HashSet<_>>();
+
+    let mut total = 0;
+
+    for page_str in pages_str.lines() {
+        let pages = page_str.split(",").collect::<Vec<_>>();
+        let mut valid = true;
+
+        for i in 0..pages.len() {
+            for j in (i + 1)..pages.len() {
+                if !tuples.contains(&(pages[i], pages[j])) {
+                    valid = false;
+                }
+            }
+        }
+
+        if valid {
+            total += pages[pages.len() / 2].parse::<i32>().unwrap();
+        }
+    }
+
+    println!("{}", total);
+    
     Ok(())
 }
